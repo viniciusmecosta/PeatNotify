dbea929b20ed8dc4b4706d44661a5b303bb14161
viniciusmecosta <costavinicius315@gmail.com>
2025-03-13 11:39:25 -0300

    feat: Email and pass from the env

--- app/constants/config.py
+++ app/constants/config.py
@@ -1,7 +1,9 @@
-EMAIL_SENDER = "costavinicius315@gmail.com"
+import os
+
+EMAIL_SENDER = os.getenv("EMAIL_SENDER", "default@gmail.com")
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_PASS = "pass"
+EMAIL_PASS = os.getenv("EMAIL_PASS", "defaltpass")
 EMAIL_SUBJECT = "Periodic Email"
 EMAIL_BODY = "This is the body of the email."
 API_URL = "http://127.0.0.1:8002"
